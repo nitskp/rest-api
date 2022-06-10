43ef9037369ba63f9c51fbfcc6de335bca0f7441
nitskp <nitinkpandey4@gmail.com>
2022-06-10 18:34:25 +0530

    Fixed services dependency error

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from '../user/user.module';
-import { UserService } from '../user/user.service';
+// import { UserService } from '../user/user.service';
 @Module({
   imports: [UserModule],
-  providers: [AuthService, UserService],
+  providers: [AuthService],
   controllers: [AuthController],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@nestjs/common';
-import { UserService } from '../user/user.service';
+import { CreateUserDto } from 'src/user/dto/createUser.dto';
+import { UserService } from 'src/user/user.service';
+import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService){}
+  constructor(private userService: UserService) {}
   async signin(email: string, password: string) {
-
+    try {
+      const user: CreateUserDto = await this.userService.findUser(email);
+      await argon.verify(user.password, password);
+      return true;
+    } catch (error) {
+      return error;
+    }
     return 'hello';
   }
 }
